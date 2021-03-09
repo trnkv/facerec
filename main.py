da74a6b2070d857b130f3666b0666298563e5534
Anna Ilina <anna.ilina.1307@yandex.ru>
2021-03-09 19:25:15 +0300

    [ADD] Функции для автоматизации поиска людей на кафедре

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ from PyQt5 import QtCore, QtGui, QtWidgets, uic
 import cv2
 import face_recognition
 
+#import pyttsx3  # синтезатор голоса
+from espeakng import ESpeakNG  # синтезатор голоса
+import speech_recognition as sr  # распознавание речи
+
 from photomanager import save_encodings_by_photos, get_encodings
 
 
@@ -22,12 +26,75 @@ QUEUE = queue.Queue()
 
 PHOTOS_PATH = 'photos/'
 ENCODINGS_PATH = 'encodings/'
+LOG_FILENAME = f"logs/log_{datetime.datetime.now().strftime('%d-%m-%y')}"
+VISITORS_FILENAME = f"logs/events/visitors_{datetime.datetime.now().strftime('%d-%m-%y')}"
+SEARCH_FILENAME = f"logs/events/search_{datetime.datetime.now().strftime('%d-%m-%y')}"
 
 START_TIME_SAVE_ENCODINGS = time.time()
 save_encodings_by_photos(PHOTOS_PATH, ENCODINGS_PATH)
 print(f'=======  Time to save encoding(s): {time.time()-START_TIME_SAVE_ENCODINGS} seconds. =======')
 
 KNOWN_FACE_ENCODINGS, KNOWN_FACE_NAMES = get_encodings(ENCODINGS_PATH)
+
+
+def say_was(search_name):
+        """
+        проходим по файлу VISITORS_FILENAME, ищем имя
+        читаем вслух последнюю строку, когда был человек
+        """
+        visitors_file = open(VISITORS_FILENAME, r)
+        all_lines = visitors_file.readlines()
+        last_string = ""
+
+        for i in range(len(all_lines)):  # line = "Иван Иванов был сегодня в 15 часов 10 минут"
+            if search_name in all_lines[i]:
+                last_string = all_lines[i]
+        
+        if last_string != "":
+            TTS.say(last_string)
+        else:
+            TTS.say(f"Я сегодня не видела {search_name}.")
+
+
+def say_find(name_ask):
+    """
+    последняя строка из файла событий:
+    {name_lookfor}, вас искал {name_ask} в *время*
+    """
+    search_file = open(SEARCH_FILENAME, r)
+    all_lines = search_file.readlines()
+    for line in all_lines:  # line = "Иван Иванов 15:10"
+        name_searcher = line.split(" ", -1)[0]  # разбиваем по последнему пробелу, получаем имя искомого чел-ка
+        time_search = line.split(" ", -1)[1]  # разбиваем по последнему пробелу, получаем время
+        if name_ask in line:
+            TTS.say(f"{name_ask}, Вас искал {name_searcher} в {time_search}.")
+
+
+def listen_forever:
+    """
+    recognize_bing(): Microsoft Bing Speech
+    recognize_google(): Google Web Speech API
+    recognize_google_cloud(): Google Cloud Speech - требует установки пакета google-cloud-speech package
+    recognize_houndify(): Houndify by SoundHound
+    recognize_ibm(): IBM Speech to Text
+    recognize_sphinx(): CMU Sphinx - требует установки PocketSphinx
+    recognize_wit(): Wit.ai
+
+    Из семи только recognize_sphinx() работает в автономном режиме с движком CMU Sphinx. Остальные шесть требуют подключения к интернету.
+    """
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+
+    with mic as source:
+        while True:
+            r.adjust_for_ambient_noise(source)  # распознает речь в шуме
+            audio = r.listen(source)  # записывает ввод от источника до тех пор, пока не будет обнаружена тишина
+            heard = r.recognize_google(audio)
+            if "Гитовна" in heard:
+                if "меня кто-то искал" in heard:
+                    say_find(...)  # передать сюда имя, полученное с распознанной фото
+                if "где" in heard:
+                    say_was(heard[4:])  # имя, полученное после слова "где"
 
 
 def grab(cam, queue, width, height):
@@ -80,6 +147,17 @@ def draw_label(frame, name, top, right, bottom, left, color):
     cv2.rectangle(frame, (left, bottom - 35), (right, bottom), color, cv2.FILLED)
     font = cv2.FONT_HERSHEY_DUPLEX
     cv2.putText(frame, name, (left + 2, bottom - 6), font, 0.5, (0, 0, 0), 1)
+
+
+def write_visitor(name):
+    global VISITORS_FILENAME
+    file_log = open(VISITORS_FILENAME, 'a+')
+    file_log.write(
+        f"{name} был сегодня в " +
+        datetime.datetime.now().strftime('%H часов %M минут') +
+        '\n')
+    file_log.close()
+
 
 
 class MyWindowClass(QtWidgets.QMainWindow, FORM_CLASS):
@@ -173,11 +251,24 @@ class MyWindowClass(QtWidgets.QMainWindow, FORM_CLASS):
                     if "Unknown" in name:
                         log = f"{datetime.datetime.now().strftime('%H:%M')} UNKNOWN: {name}"                    
                         QT_textBrowser.append(log)
+                        write_visitor(name) # записать в журнал посещений
+                        # TODO: спросить имя, записать, узнать, что хотел, записать в журнал событий
                     else:
                         log = f"{datetime.datetime.now().strftime('%H:%M')} Detected: {name}"
                         QT_textBrowser.append(log)
+                        write_visitor(name) # записать в журнал посещений
+                        current_hour = int(datetime.datetime.now().strftime('%H'))
+                        if current_hour >= 0 and current_hour < 12:
+                            TTS.say(f"Доброе утро, {name}!")
+                        elif current_hour >=12 and current_hour < 17:
+                            TTS.say(f"Добрый день, {name}!")
+                        elif current_hour >=17 and current_hour < 0:
+                            TTS.say(f"Добрый вечер, {name}!")
+                        # TODO: проверить, не искал ли его кто-то; если да - сказать
+                        say_find(name)
                     # создаем лог-файл для текущей сессии
-                    file_log = open(f"logs/log_{datetime.datetime.now().strftime('%d-%m-%y')}", 'a+')
+                    global LOG_FILENAME
+                    file_log = open(LOG_FILENAME, 'a+')
                     file_log.write(log+'\n')
                     file_log.close()
                     draw_label(img, name, top, right, bottom, left, (0, 255, 0))
@@ -196,6 +287,11 @@ class MyWindowClass(QtWidgets.QMainWindow, FORM_CLASS):
 
 
 CAPTURE_THREAD = threading.Thread(target=grab, args=(0, QUEUE, 200, 200))
+
+# TTS = pyttsx3.init()
+TTS = ESpeakNG()
+TTS.speed(150)
+TTS.voice = 'russian'
 
 APPLICATION = QtWidgets.QApplication(sys.argv)
 WINDOW = MyWindowClass(None)
